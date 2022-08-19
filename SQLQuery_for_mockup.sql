9f233e3f3b6b28a4d705086f1906059a1b9d1bdc
Tuan Bui <buiductuan01@gmail.com>
2022-08-19 09:35:44 +0700

    sche_mockup

--- SQLQuery_for_mockup.sql
+++ SQLQuery_for_mockup.sql
@@ -1,1 +1,3 @@
 SELECT CONCAT('NV',RIGHT(CONCAT('000',Teacher.id),4)) as 'ID' ,Teacher.name, Position.name as 'Pos' FROM Teacher, Position WHERE Teacher.posId = Position.id
+
+SELECT tsd.[start], tsd.[end] FROM [Time Slot] ts,[Time Slot Detail] tsd WHERE ts.id = tsd.timeSlotId AND ts.isActive = 1 AND tsd.isActive = 1
